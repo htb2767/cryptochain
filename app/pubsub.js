94baeb6dba7dd2c32382c9500a60322838bdffec
Huong Bui <hbui23@gmu.edu>
2023-12-19 16:30:46 -0500

    switch to pubnub

--- app/pubsub.js
+++ app/pubsub.js
@@ -1,4 +1,11 @@
-const redis =require('redis');
+//const redis =require('redis');
+const PubNub = require('pubnub');
+
+const credentials = {
+  publishKey: 'pub-c-e24a56b5-186e-4577-951b-7e145eddf6f5',
+  subscribeKey: 'sub-c-9cf368ff-56ca-4768-9b0a-b49d1b363f7f',
+  secretKey: 'sec-c-ZmE5MzQyNjAtNTQ2Yi00NGQ4LWEyMWYtYmJmMzRhYjA0ODRl'
+};
 const { parse } = require('uuid');
 const CHANNELS={
     TEST: 'TEST',
@@ -10,11 +17,14 @@ class PubSub{
     constructor({blockchain,transactionPool}){
         this.blockchain=blockchain;
         this.transactionPool=transactionPool;
-        this.publisher=redis.createClient();
-        this.subscriber=redis.createClient();
-        this.subscribeToChannels();
-        this.subscriber.on(
-            'message',(channel,message)=>this.handleMessage(channel,message));
+        // this.publisher=redis.createClient();
+        // this.subscriber=redis.createClient();
+        // this.subscribeToChannels();
+        // this.subscriber.on(
+        //     'message',(channel,message)=>this.handleMessage(channel,message));
+        this.pubnub = new PubNub(credentials);
+        this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+        this.pubnub.addListener(this.listener());
 
     }
     handleMessage(channel,message){
@@ -39,18 +49,28 @@ class PubSub{
         
 
     }
-    subscribeToChannels(){
-        Object.values(CHANNELS).forEach(channel=>{
-            this.subscriber.subscribe(channel);
-        });
-    }
+    // subscribeToChannels(){
+    //     Object.values(CHANNELS).forEach(channel=>{
+    //         this.subscriber.subscribe(channel);
+    //     });
+    // }
+    listener() {
+        return {
+          message: messageObject => {
+            const { channel, message } = messageObject;
+    
+            this.handleMessage(channel, message);
+          }
+        };
+      }
     publish({channel,message}){
-        this.subscriber.unsubscribe(channel,()=>{
-            this.publisher.publish(channel,message,()=>{
-                this.subscriber.subscribe(channel,message);
+        // this.subscriber.unsubscribe(channel,()=>{
+        //     this.publisher.publish(channel,message,()=>{
+        //         this.subscriber.subscribe(channel,message);
 
-            })
-        })
+        //     })
+        // })
+        this.pubnub.publish({ channel, message });
         
     }
     broadcastChain(){
